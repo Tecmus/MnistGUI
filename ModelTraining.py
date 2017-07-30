e155345a101f504255294e653d283926e8858fbc
tecmus <tecmus@aliyun.com>
2017-07-30 19:52:07 +0800

    delete t.py

--- ModelTraining.py
+++ ModelTraining.py
@@ -1,82 +1,115 @@
-#coding=utf-8
-from __future__ import print_function
-import keras
 from keras.datasets import mnist
-from keras.layers import Dense, Flatten
-from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Sequential
-import matplotlib.pylab as plt
+from keras.models import load_model
 
-batch_size = 128
-num_classes = 10
-epochs = 5
+import keras
+from keras.layers import Dense,Activation,Dropout
+from keras.layers import Conv2D, MaxPooling2D,Flatten
 
-# input image dimensions
-img_x, img_y = 28, 28
-
-# load the MNIST data set, which already splits into train and test sets for us
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# reshape the data into a 4D tensor - (sample_number, x_img_size, y_img_size, num_channels)
-# because the MNIST is greyscale, we only have a single channel - RGB colour images would have 3
-x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 1)
-x_test = x_test.reshape(x_test.shape[0], img_x, img_y, 1)
-input_shape = (img_x, img_y, 1)
-
-# convert the data to the right type
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices - this is for use in the
-# categorical_crossentropy loss below
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-model = Sequential()
-#32个 filters k-size filter matrix 
-model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
-                 activation='relu',
-                 input_shape=input_shape))
-
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-model.add(Conv2D(64, (5, 5), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-
-#fully connect nn 
-model.add(Dense(1000, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
-
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adam(),
-              metrics=['accuracy'])
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-class AccuracyHistory(keras.callbacks.Callback):
-    def on_train_begin(self, logs={}):
-        self.acc = []
+class MnistTraining:
+    def __init__(self,model_type="cnn"):
 
-    def on_epoch_end(self, batch, logs={}):
-        self.acc.append(logs.get('acc'))
+        self.model_type=model_type
+        self.model_path = "./models/"+model_type
+        print(self.model_type)
+        print(self.model_path)
 
-history = AccuracyHistory()
+    def processData(self,model_type='cnn'):
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test),
-          callbacks=[history])
-model.save("D:/cnn")
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-plt.plot(range(1, 11), history.acc)
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.show()
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        if model_type=="cnn":
+            model_path="./models/cnn"
+            x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+            x_test = x_test.reshape(x_test.shape[0], x_train.shape[1], x_train.shape[2], 1)
+            self.cnn_input_shape = (x_train.shape[1], x_train.shape[2], 1)
+
+            # convert the data to the right type
+            x_train = x_train.astype('float32')
+            x_test = x_test.astype('float32')
+            x_train /= 255
+            x_test /= 255
+            y_train = keras.utils.to_categorical(y_train, 10)
+            y_test = keras.utils.to_categorical(y_test, 10)
+
+        elif model_type=="mlp":
+            model_path = "./models/mlp"
+            x_train = x_train.reshape(x_train.shape[0], x_train.shape[1] * x_train.shape[2])
+            x_test = x_test.reshape(x_test.shape[0], x_test.shape[1] * x_test.shape[2])
+            y_train = (np.arange(10) == y_train[:, None]).astype(int)
+            y_test = (np.arange(10) == y_test[:, None]).astype(int)
+
+        return x_train,y_train,x_test,y_test
+
+    def cnnTrain(self,epoch_num=10):
+        x,y,x_,y_=self.processData("cnn")
+        self.model = Sequential()
+
+        self.model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
+                         activation='relu',
+                         input_shape=self.cnn_input_shape))
+
+        self.model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        self.model.add(Conv2D(64, (5, 5), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Flatten())
+
+        # fully connect nn 
+        self.model.add(Dense(1000, activation='relu'))
+        self.model.add(Dense(10, activation='softmax'))
+
+        self.model.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adam(),
+                      metrics=['accuracy'])
+
+        self.result=self.model.fit(x, y,
+                  batch_size=32, epochs=epoch_num, verbose=1)
+
+    def mlpTrain(self,epoch_num):
+        x,y,x_,y_=self.processData('mlp')
+        self.model=Sequential([
+            Dense(units=500,input_shape=(784,)),
+            Activation("relu"),
+            Dense(units=100),
+            Activation("sigmoid"),
+            Dense(10),
+            Activation("softmax")
+        ])
+        self.model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.result=self.model.fit(x,y,epochs=epoch_num,batch_size=120,verbose=1) # 1 with log
+
+
+
+    def train(self,epochs=10):
+
+        if self.model_type=='cnn':
+            self.processData('cnn')
+            self.cnnTrain(epoch_num=epochs)
+            pass
+        elif self.model_type=='mlp':
+            self.processData('mlp')
+            self.mlpTrain(epoch_num=epochs)
+            pass
+
+        print(self.result.history)
+        self.draw(self.result)
+        self.model.save(self.model_path)
+
+        pass
+
+    def draw(self,res):
+        x =range(len(res.history['acc']))
+        plt.plot(x, res.history['acc'])
+        plt.show()
+        pass
+if __name__=='__main__':
+
+    mt=MnistTraining("cnn") #change 'cnn' to 'mlp' to use fully connect network
+    mt.train(5)
+
+
+
+
